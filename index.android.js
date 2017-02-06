30742b962d1f993b551a9f71fa4897c08a35f747
dbertella <daniele.bertella@gmail.com>
2017-02-06 17:51:46 +0000

    initial commit

---
+++ index.android.js
@@ -0,0 +1,78 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Text,
+  ListView,
+  View
+} from 'react-native';
+import styled from 'styled-components/native';
+
+const Row = styled.View`
+    flex: 1;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+const data = [{
+    name: 'Test 1',
+    date: '19th of January',
+    body: 'Test Test Test Test Test'
+  }, {
+    name: 'Test 2',
+    date: '19th of January',
+    body: 'Test Test Test Test Test'
+  }, {
+    name: 'Test 3',
+    date: '19th of January',
+    body: 'Test Test Test Test Test'
+  }, {
+    name: 'Test 4',
+    date: '19th of January',
+    body: 'Test Test Test Test Test'
+  }, {
+    name: 'Test 5',
+    date: '19th of January',
+    body: 'Test Test Test Test Test'
+  },
+]
+
+const Line = (rowData) => (
+  <View style={{ borderBottomWidth: 1 }}>
+    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+      <Text>{rowData.name}</Text>
+      <Text>{rowData.date}</Text>
+    </View>
+    <Row>
+      <Text>{rowData.name}</Text>
+      <Text>{rowData.date}</Text>
+    </Row>
+    <Text>{rowData.body}</Text>
+  </View>
+)
+
+export default class RNstyledComponent extends Component {
+   constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(data),
+    };
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={Line}
+      />
+    );
+  }
+}
+
+AppRegistry.registerComponent('RNstyledComponent', () => RNstyledComponent);
